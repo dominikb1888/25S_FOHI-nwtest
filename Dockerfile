94172b452bfa3fbcb2126a68da66e147c0318d83
Dominik Böhler <dominik.boehler@gmx.net>
2025-05-05 13:05:32 +0200

    added fhir bundles

--- Dockerfile
+++ Dockerfile
@@ -8,11 +8,11 @@ COPY requirements.txt .
 RUN pip install --no-cache-dir -r requirements.txt
 
 # Copy app code
-COPY app/ ./app/
+COPY app/* .
 
 # Expose port
 EXPOSE 8000
 
 # Run the application
-CMD ["uvicorn", "app.main:app", "--host", "0.0.0.0", "--port", "8000", "--reload"]
+CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "8000", "--reload"]
 
--- app/main.py
+++ app/main.py
@@ -1,14 +1,29 @@
 from fastapi import FastAPI
+
 import redis
+from redis.commands.json.path import Path
+
+import json
+import os
 
 app = FastAPI()
 
 # Connect to Redis
 r = redis.Redis(host="redis", port=6379, decode_responses=True)
 
+folder_path = "fhir"
+for file in os.listdir(folder_path):
+    fhir_patient_file = os.path.join(folder_path, file)
+    with open(fhir_patient_file, 'rb') as f:
+        fhir_patient = json.load(f)
+
+    uuid = fhir_patient["entry"][0]["fullUrl"]
+    r.json().set(uuid, Path.rootPath(), fhir_patient)
+
+
 @app.get("/")
 def read_root():
-    # Example: increment a counter in Redis
-    count = r.incr("counter")
-    return {"Hello": "World", "counter": count}
+    keys = r.keys()
+    return [r.get(key) for key in keys]
+
 
